f00ffd173a9cb760585f57f01ae2f29cb047ce54
MarcMerino <marcmerino24@gmail.com>
2021-02-25 20:47:56 +0100

    First commit

---
+++ www/js/index.js
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+
+let list = $("ul")[0];
+
+addOnClickListeners();
+
+function addOnClickListeners() {
+    $(".close").click(function(){
+        console.log("CLOSEEED!");
+    });
+}
+
+addTask();
+addTask();
+addTask();
+
+
+function addTask() {
+    let li = document.createElement("li");
+    let t = document.createTextNode("inputValue");
+    li.appendChild(t);
+  
+    let span = document.createElement("span");
+    let txt = document.createTextNode("x");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    list.appendChild(li);
+
+    addOnClickListeners();
+  } 
