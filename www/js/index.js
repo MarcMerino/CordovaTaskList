dcd357c6bfeaecf6c2cda0cbcd98ea215e5b5a0a
MarcMerino <marcmerino24@gmail.com>
2021-02-27 18:40:31 +0100

    Fixed 99% problems

--- www/js/index.js
+++ www/js/index.js
@@ -20,44 +20,60 @@
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 
-let list = $("ul")[0];
-let addTaskButton = $(".buttonTask").click(function(){
-    addTask();
+let liList = [];
+let addTaskButton = $(".buttonTask").click(function(){addTask();});
+let inputField = $(".inputTask");
+
+inputField.on('keyup', function(e) {
+    if (e.key === "Enter") {
+        addTask();
+    }
 });
-
-// getStructure();
-
-addTask();
-
-addOnClickListeners();
 
 function getStructure() {
     if (localStorage.getItem("listEntries") != null) {
-        localStorage.getItem("listEntries").li.forEach(i => {
+        JSON.parse(localStorage.getItem("listEntries")).li.forEach(i => {
             addTask(i);
         });
     }
+
+    $("ul").listview().listview('refresh');
+
+    saveActualList();
 }
 
 function addOnClickListeners() {
     $(".close").click(function(){
-        $(this).parent().remove()
-        $("ul").listview('refresh');
+        $(this).parent().remove();
+        $("ul").listview().listview('refresh');
+        saveActualList();
     });
 }
 
 function addTask(element = null) {
-    let value = $(".inputTask")[0].value;
-
+    let value = inputField[0].value;
     if (element === null && value !== "") {
-        $("ul").append('<li>' + value + '<span class="close">x</span></li>')
+        $("ul").append('<li class="hoverEffect">' + value + '<span class="close">x</span></li>')
     } else if (element !== null) {
-        $("ul").append('<li>' + element + '<span class="close">x</span></li>')
+        $("ul").append('<li class="hoverEffect">' + element + '<span class="close">x</span></li>')
     }
 
-    $("ul").listview('refresh');
-
-    // localStorage.setItem("listEntries", JSON.stringify({"li": $("ul").children().toArray().firstChild.data}))
+    inputField[0].value = "";
+    saveActualList();
+   
+    $("ul").listview().listview('refresh');
 
     addOnClickListeners();
 }
+
+function saveActualList() {
+    liList = [];
+    $("ul").children().toArray().forEach(i => {
+        liList.push(i.firstChild.data);
+    });
+
+    localStorage.setItem("listEntries", JSON.stringify({"li": liList}))
+}
+
+getStructure();
+addOnClickListeners();
